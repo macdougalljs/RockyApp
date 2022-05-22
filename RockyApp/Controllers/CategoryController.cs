ac2f544cb9a23982aa9aa54b0cea5839d6074565
Jeremy MacDougall <macdojer@outlook.com>
2022-05-22 18:09:33 -0400

    Add project files.

---
+++ RockyApp/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockyApp.Data;
+
+namespace RockyApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        // the addDbContext was already added in Startup.Cs, so we create a Constructor
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)  // this generates an INSTANCE of the DbContext from the DI
+        {
+        _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RockyApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RockyApp.Models;
+
+namespace RockyApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category
+        {
+            get; set;
+        }
+    }
+}
